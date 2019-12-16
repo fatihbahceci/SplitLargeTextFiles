52d161dd17df973795b4449313c89d4b4c967051
fatih <fatihbahceci@gmail.com>
2019-12-16 13:13:47 +0300

    First Release

--- FileSplitter/Properties/AssemblyInfo.cs
+++ FileSplitter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FileSplitter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Large File Splitter")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("fatihbahceci@gmail.com")]
 [assembly: AssemblyProduct("FileSplitter")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+//yyyy-MM-dd
+//2019-12-16 1.0.0.1: First release
